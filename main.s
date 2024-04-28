ecdbc9537212821739a08d0dd00667df317dbfe9
Mister1SI <wazzup2690@gmail.com>
2024-04-27 21:15:09 -0500

    Segfaulting #1

--- main.s
+++ main.s
@@ -1,6 +1,9 @@
 .data
 	msg: .asciz "Hello\n"
 
+.bss
+	filename: .skip 4096
+
 .text
 .global _start
 .global z_end
@@ -14,12 +17,12 @@ _start:
 	cmp x0, #1				// Check for 0 args
 	b.ne arg_ne_1			// If argc != 1 jump to reading the file
 	adr x30, z_end			// Change link register to z_end
-	b _print_help
-	arg_ne_1:				// File supplied; open, read, and mmap
+	b _print_help			// Jump to _print_help, lr wil return to z_end
 	
+	arg_ne_1:				// File supplied; open, read, and mmap
+	ldr w2, [x1]			// Load the first element of argv
 
-
-	adr x0, msg
+	mov x0, x2				// Print the argument
 	bl _prints
 
 
