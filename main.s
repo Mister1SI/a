844159bd9712a421efcacec45f3db6777bf26f27
Mister1SI <wazzup2690@gmail.com>
2024-04-30 08:00:51 -0500

    Not work

--- main.s
+++ main.s
@@ -3,6 +3,7 @@
 
 .bss
 	filename: .skip 4096
+	cwd: .skip 4096
 
 .text
 .global _start
@@ -13,18 +14,44 @@
 
 _start:
 	
-	ldp x0, x1, [sp]		// Load argc (x0) and argv (x1)
+	ldp x0, x1, [sp]		// Load argc (x0) and the first argument (x1)
+	ldp x2, x3, [sp, #0x10]	// Load the second and third arguments
 	cmp x0, #1				// Check for 0 args
 	b.ne arg_ne_1			// If argc != 1 jump to reading the file
 	adr x30, z_end			// Change link register to z_end
 	b _print_help			// Jump to _print_help, lr wil return to z_end
 	
 	arg_ne_1:				// File supplied; open, read, and mmap
-	ldr w2, [x1]			// Load the first element of argv
-
 	mov x0, x2				// Print the argument
 	bl _prints
 
+	mov x8, #17				// getcwd
+	adr x0, cwd				// Buffer
+	mov x1, #4096			//
+	svc #0
+
+	mov x8, #56				// openat
+	adr x0, cwd				// dirfd
+	mov x1, x2				// Filename
+	mov x2, #0x0			// Flags
+	mov x3, #0644			// Mode
+	svc #0
+	mov x19, x0				// Store fd in x19
+
+	mov x8, #222			// mmap
+	mov x0, xzr				// Supply no address
+	mov x1, xzr				// Length
+	mov x2, xzr				// prot
+	mov x3, xzr				// flags
+	mov x4, x19				// fd
+	mov x5, xzr				// offset
+	svc #0
+	mov x20, x0				// Store void* into x20
+
+
+	mov x8, #57				// close
+	mov x0, x19				// fd
+	svc #0
 
 	z_end:
 	mov x8, #93
